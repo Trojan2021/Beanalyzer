809dd0ecf7d2231f47f9023d3175dd9722a712b1
AnotherMichael8 <130487522+AnotherMichael8@users.noreply.github.com>
2024-10-19 17:11:47 -0400

    Friend viewer works

--- Android App/app/src/main/java/com/example/breathalyzer_app/FriendAdpater.kt
+++ Android App/app/src/main/java/com/example/breathalyzer_app/FriendAdpater.kt
@@ -3,24 +3,45 @@ package com.example.breathalyzer_app
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 
 class FriendAdpater (
     private val friends: MutableList<Friend>
 ):RecyclerView.Adapter<FriendAdpater.FriendViewHolder>(){
+
+    class FriendViewHolder(itemView : View) : RecyclerView.ViewHolder(itemView) {
+        val tvFriendTrackComp: TextView = itemView.findViewById(R.id.tvFriendTrackComp)
+        val tvPlayerBac : TextView = itemView.findViewById(R.id.tvPlayerBAC)
+    }
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): FriendViewHolder {
         return FriendViewHolder(
-            LayoutInflater.from(parent.context).inflate(R.layout.singe_friend_view, parent, false)
+            LayoutInflater.from(parent.context).inflate(R.layout.single_friend_view, parent, false)
         )
+    }
+    fun addFriend(friend: Friend)
+    {
+        friends.add(friend)
+        notifyItemInserted(friends.size - 1)
+    }
+    fun deleteFriend(friend: Friend)
+    {
+        friends.remove(friend)
+        notifyDataSetChanged()
     }
 
     override fun onBindViewHolder(holder: FriendViewHolder, position: Int) {
-
+        val curFriend = friends[position]
+        holder.apply{
+            tvFriendTrackComp.text = curFriend.friend
+            tvPlayerBac.text = curFriend.bac
+        }
     }
 
     override fun getItemCount(): Int {
         return friends.size
     }
 
-    class FriendViewHolder(itemView : View) : RecyclerView.ViewHolder(itemView)
+    //class FriendViewHolder(itemView : View) : RecyclerView.ViewHolder(itemView)
 }
--- Android App/app/src/main/java/com/example/breathalyzer_app/FriendViewActivity.kt
+++ Android App/app/src/main/java/com/example/breathalyzer_app/FriendViewActivity.kt
@@ -1,12 +1,18 @@
 package com.example.breathalyzer_app
 
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class FriendViewActivity : AppCompatActivity() {
+
+    private lateinit var friendAdapter : FriendAdpater
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -15,6 +21,23 @@ class FriendViewActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+        val rvListOfFriends = findViewById<RecyclerView>(R.id.rvFriendView)
+        val addFriend = findViewById<Button>(R.id.btnAddFriend)
+
+        friendAdapter = FriendAdpater(mutableListOf())
+        rvListOfFriends.adapter = friendAdapter
+        rvListOfFriends.layoutManager = LinearLayoutManager(this)
+
+        addFriend.setOnClickListener {
+            val friendName = "TESTING"
+            val bac = "0.08"
+            if(friendName.isNotEmpty())
+            {
+                val friend = Friend(friendName, bac)
+                friendAdapter.addFriend(friend)
+            }
+
         }
     }
 }
